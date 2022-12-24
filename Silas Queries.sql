067c2c6bf893c423a20c50d6a948e01b21001d85
lunettas <silaslunetta@gmail.com>
2022-12-23 23:38:19 -0700

    new stability queries/ typo

--- Silas Queries.sql
+++ Silas Queries.sql
@@ -2,3 +2,8 @@ Use discs;
 SELECT *
 FROM(SELECT * FROM silasdiscs WHERE Color = 'White') AS whitediscs
 WHERE Category = 'Side Bag' OR Category = 'Main Bag';
+
+SELECT DISTINCT Mold, Speed, Glide, Turn, Fade, `Sleepy Scale`, Weight, Stability FROM
+(SELECT Mold, Speed, Glide, Turn, Fade, `Sleepy Scale`, Weight, (Turn + Fade) AS Stability  FROM silasDiscs ORDER BY Stability) AS calStability
+WHERE Weight !='Null'
+ORDER BY Stability, Weight,`Sleepy Scale` DESC;

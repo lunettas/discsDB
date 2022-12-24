6d7d2867ab139294e849ab2ab16d4c052e07651e
Josh Cabral <55261816+joshycsm@users.noreply.github.com>
2022-12-24 01:29:27 -0700

    to be cont. :)

--- JCINVENTORY_122322.sql
+++ JCINVENTORY_122322.sql
@@ -33,12 +33,34 @@ VALUES
     
     ('Zone', 'Z Cryztal Flx Glo', 'DISCRAFT', 173, 4, 3, 0, 3, 'Putter', 'Backup', 'Orange', 'Get Freaky - Metallic Purple', 7),
     
+
+
+
+
+
+
+
+    ('Diamond', 'Opto Air', 'LATITUDE 64°', 139, 8, 6, -3, 1, 'Fairway Driver', 'Backup', 'Teal', 'Bronze', 5),
+
+
+    ('Wraith', 'Star', 'INNOVA DISC GOLF', 162, 11, 5, -1, 3, 'Distance Driver', 'Backup', 'Orange', '12 Time World Champion Ken Climo - Black', 6),
+    ('Wraith', 'Star', 'INNOVA DISC GOLF', 175, 11, 5, -1, 3, 'Distance Driver', 'Backup', 'Light Orange', 'No Stamp', 6),
+    ('Tern', 'Star', 'INNOVA DISC GOLF', 170, 12, 6, -2, 2, 'Distance Driver', 'Backup', 'Orange', 'Santa Cruz Hand Mouth', 7),
+    ('Mystere', 'Star', 'INNOVA DISC GOLF', 171, 11, 6, -2, 2, 'Distance Driver', 'Backup', 'Purple', 'Santa Cruz Hand Mouth', 7),
+
+
+
+
+
+
+    ('Bolt', 'Gold Line Orbit', 'LATITUDE 64°', 175, 13, 6, -2, 3, 'Distance Driver', 'Backup', 'Red', '10 Year Anniversary', 7),
+    ('Bolt', 'Gold Line Orbit', 'LATITUDE 64°', 175, 13, 6, -2, 3, 'Distance Driver', 'Backup', 'Orange', '10 Year Anniversary', 5),
+    ('Bolt', 'Gold Line Orbit', 'LATITUDE 64°', 175, 13, 6, -2, 3, 'Distance Driver', 'Backup', 'Red', '10 Year Anniversary', 8),
+    ('Bolt', 'Gold Line Orbit', 'LATITUDE 64°', 175, 13, 6, -2, 3, 'Distance Driver', 'Backup', 'Orange', '10 Year Anniversary', 8),
+    ('Bolt', 'Silas', 'LATITUDE 64°', 174, 13, 6, -2, 3, 'Distance Driver', 'Backup', 'Pink Glimmer', 'Silas', 4),
+
     ('Sapphire', 'Opto', 'LATITUDE 64°', 175, 10, 6, -2, 1.5, 'Control', 'Emily', 'Orange', 'Silver', 7),
-    
 
-
-
-    
     ('Judge', 'Lucid', 'DYNAMIC DISCS', 176, 2, 4, 0, 1, 'Putter', 'Loan', 'Tie Die', 'Purple', 7),
     ('Judge', 'Lucid', 'DYNAMIC DISCS', 176, 2, 4, 0, 1, 'Putter', 'Loan', 'Yellow Speckle', 'Purple', 7),
     ('Judge', 'Fuzion', 'DYNAMIC DISCS', 176, 2, 4, 0, 1, 'Putter', 'Loan', 'Pink', '2018 World Champion Paige Shue - Darler Blue', 7),
@@ -46,23 +68,24 @@ VALUES
     ('Harp', 'BT Soft', 'WESTSIDE DISCS', 174, 4, 3, 0, 3, 'Putter', 'Loan', 'Yellow', 'Red', 7),
     ('Harp', 'BT Medium', 'WESTSIDE DISCS', 173, 4, 3, 0, 3, 'Putter', 'Loan', 'Blue', 'Physics of Flight - Gold', 7),
     ('Harp', 'UNKNOWN', 'WESTSIDE DISCS', 176, 4, 3, 0, 3, 'Putter', 'Loan', 'Clear', 'Ricky Wysocki 2x World Champion - Red', 7),
-    ('Enforcer', 'Lucid', 'DYNAMIC DISCS', 172, 12, 4, 0.5, 4, 'Driver', 'Loan', 'Blue', 'Gold Woodgrain', 7),
-    ('Enforcer', 'Fuzion Bio', 'DYNAMIC DISCS', 172, 12, 4, 0.5, 4, 'Driver', 'Loan', 'Yellow', 'Blue', 7),
-    ('Ballista Pro', 'Opto', 'LATITUDE 64°', 174, 14, 4, 0, 3, 'Driver', 'Loan', 'Pink Glimmer', `PDGA Pro Worlds '21 - Black`, 7),
-    ('Defender', 'Fuzion', 'DYNAMIC DISCS', 172, 13, 5, 0, 3, 'Driver', 'Loan', 'Orange', 'Silver Holographic', 7),
+    ('Enforcer', 'Lucid', 'DYNAMIC DISCS', 172, 12, 4, 0.5, 4, 'Distance Driver', 'Loan', 'Blue', 'Gold Woodgrain', 7),
+    ('Enforcer', 'Fuzion Bio', 'DYNAMIC DISCS', 172, 12, 4, 0.5, 4, 'Distance Driver', 'Loan', 'Yellow', 'Blue', 7),
+    ('Ballista Pro', 'Opto', 'LATITUDE 64°', 174, 14, 4, 0, 3, 'Distance Driver', 'Loan', 'Pink Glimmer', `PDGA Pro Worlds '21 - Black`, 7),
+    ('Defender', 'Fuzion', 'DYNAMIC DISCS', 172, 13, 5, 0, 3, 'Distance Driver', 'Loan', 'Orange', 'Silver Holographic', 7),
     
-    ('Valkyrie', 'DX', 'INNOVA DISC GOLF', 175, 9, 4, -2, 2, 'Control', 'Loan', 'Pink', 'Silver', 5),
-    ('Leopard', 'DX', 'INNOVA DISC GOLF', 149, 6, 5, -2, 1, 'Control', 'Loan', 'Pink', 'Silver', 5),
+    
+    ('Valkyrie', 'DX', 'INNOVA DISC GOLF', 175, 9, 4, -2, 2, 'Control Driver', 'Loan', 'Pink', 'Silver', 5),
+    ('Leopard', 'DX', 'INNOVA DISC GOLF', 149, 6, 5, -2, 1, 'Control Driver', 'Loan', 'Pink', 'Silver', 5),
     ('PD', 'D-Line', 'DISCMANIA', 171, 10, 4, 0, 3, 'Control Driver', 'Loan', 'White', 'X-Out - Black', 5),
     ('Beast', 'DX', 'INNOVA DISC GOLF', 168, 10, 5, -2, 2, 'Control Driver', 'Loan', 'White', 'Gold', 4),
     ('Wraith', 'DX', 'INNOVA DISC GOLF', 175, 11, 5, -1, 3, 'Distance Driver', 'Loan', 'Orange', 'White', 5),
+    ('Renegade', 'Champion Glow', 'INNOVA DISC GOLF', 176, 11, 5, -1.5, 2.5, 'Distance Driver', 'Loan', 'Pink', 'Silver', 5),
     
     ('Archangel', 'DX', 'INNOVA DISC GOLF', 171, 11, 5, -1, 3, 'Distance Driver', 'Sale / Trade', 'Orange', 'Blue', 5),
     ('Lace', 'Vibram', 'UNKNOWN', 165, 14, 6, -1, 1, 'Distance Driver', 'Sale / Trade', 'White', 'Rainbow Speckles', 4),
     
     
     ('UNKNOWN', 'DX', 'UNKNOWN', 171, 8, 5, -2, 2, 'Control', 'Sale / Trade', 'Orange', 'Green', 5),
-    ('Valkyrie', 'DX', 'INNOVA DISC GOLF', 175, 9, 4, -2, 2, 'Control', 'Sale / Trade', 'Pink', 'Silver', 5)
 	;
 
 -- 'Main Bag', 'Side Bag', 'Collection', 'Backup', 'Emily', 'Sale / Trade', 'Loan'
@@ -70,6 +93,8 @@ select * from silasdiscs;
 select * from silasdiscs where mold = 'Fierce';
 select * from jcdiscs;
 select * from jcdiscs where mold = 'Fierce';
+
+-- 'Putter', 'Mid-Range', 'Fairway Driver' , 'Control Driver', 'Distance Driver'
 
 -- 1-4 Putter
 -- 5-6 Mid

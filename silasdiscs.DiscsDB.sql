c0daa0a3163f8ad31d8cc716eeaf342afc3ee82b
lunettas <silaslunetta@gmail.com>
2022-12-23 17:34:14 -0700

    Saving before pull

--- silasdiscs.DiscsDB.sql
+++ silasdiscs.DiscsDB.sql
@@ -7,11 +7,11 @@ Brand VARCHAR(100),
 Weight INT,
 Speed INT,
 Glide INT,
-Turn DECIMAL(5,2),
-Fade DECIMAL(5,2),
+Turn DECIMAL(2,1),
+Fade DECIMAL(2,1),
 Slot ENUM( 'Putter', 'Mid-Range', 'Fairway Driver' , 'Control Driver', 'Distance Driver'),
-Bag ENUM('Main Bag', 'Side Bag', 'Collection', 'Backup', 'Emily', 'For Sale', 'Silas Trade', 'JC Trade'), 
+Category ENUM('Main Bag', 'Side Bag', 'Collection', 'Backup', 'Emily', 'Sale / Trade'), 
 Color VARCHAR(100),
 Stamp VARCHAR(100),
-SleepyScale INT
+`SleepyScale` INT
 );

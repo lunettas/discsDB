fef91eb56080a7b7234f77e9666ee47467153788
Josh Cabral <55261816+joshycsm@users.noreply.github.com>
2022-12-26 11:42:18 -0700

    good inventory done :)

--- JCINVENTORY_122322.sql
+++ JCINVENTORY_122322.sql
@@ -17,7 +17,6 @@ VALUES
     ('Fierce', 'Z Swirl', 'DISCRAFT', 174, 3, 4, -2, 0, 'Putter', 'Backup', 'Gray-Green', '2022 Ledgestone Edition - Green Lasers', 7),
     ('Fierce', 'Z Swirl', 'DISCRAFT', 172, 3, 4, -2, 0, 'Putter', 'Backup', 'Yellow', '2022 Ledgestone Edition - Gold Holographic - black specks', 7),
     ('Fierce', 'Z Swirl', 'DISCRAFT', 172, 3, 4, -2, 0, 'Putter', 'Backup', 'Yellow1', '2022 Ledgestone Edition - Gold Holographic', 7),
-    ('Fierce', 'Big Z', 'DISCRAFT', 172, 3, 4, -2, 0, 'Putter', 'Side Bag', 'Orange', '2021 Ledgestone Edition - Orange', 5),
     ('Fierce', 'Big Z', 'DISCRAFT', 173, 3, 4, -2, 0, 'Putter', 'Backup', 'Red', '2021 Ledgestone Edition - Purple', 7),
     ('Fierce', 'Big Z', 'DISCRAFT', 173, 3, 4, -2, 0, 'Putter', 'Backup', 'Green', '2021 Ledgestone Edition - Maroon', 7),
     ('Fierce', 'Big Z', 'DISCRAFT', 173, 3, 4, -2, 0, 'Putter', 'Backup', 'Pink', '2021 Ledgestone Edition - Green Star', 7),
@@ -32,17 +31,40 @@ VALUES
     ('Tomb', 'Swirly S-Blend', 'INFINITE DISCS', 173, 3, 4, 0, 1, 'Putter', 'Backup', 'Yellow', 'Black', 7),
     
     ('Zone', 'Z Cryztal Flx Glo', 'DISCRAFT', 173, 4, 3, 0, 3, 'Putter', 'Backup', 'Orange', 'Get Freaky - Metallic Purple', 7),
-    
 
 
 
+    ('Tomb', 'Glow D-Blend', 'INFINITE DISCS', 173, 3, 4, 0, 1, 'Putter', 'Side Bag', 'Blue', 'Ghost Stamp - Metallic Purple', 7),
+    ('Tomb', 'I-Blend', 'INFINITE DISCS', 173, 3, 4, 0, 1, 'Putter', 'Side Bag', 'Red', 'Silver', 6),
+    ('Fierce', 'Special Blend', 'DISCRAFT', 174, 3, 4, -2, 0, 'Putter', 'Side Bag', 'Yellow', 'Rainbow Shatter', 6),
+    ('Fierce', 'Big Z', 'DISCRAFT', 172, 3, 4, -2, 0, 'Putter', 'Side Bag', 'Orange', '2021 Ledgestone Edition - Orange', 5),
+    ('Zone', 'Z', 'DISCRAFT', 174, 4, 3, 0, 3, 'Putter', 'Side Bag', 'Purple', 'Paul McBeth - 5X World Champion - Silver Daisy', 6),
+    ('Fuse', 'Gold-X', 'LATITUDE 64°', 173, 5, 6, -1, 0, 'Mid-Range', 'Side Bag', 'Yellow', 'John E McCray Team Series V.2 2022'- Metallic Blue, 6),
+    ('Justice', 'Lucid Plastic', 'DYNAMIC DISCS', 163, 5, 1, 0.5, 4, 'Mid-Range', 'Side Bag', 'Pink', '2022 Dynamic Discs Open - Rainbow', 6),
+    ('Diamond', 'Gold Line Orbit', 'LATITUDE 64°', 151, 8, 6, -3, 1, 'Fairway Driver', 'Side Bag', 'Purple', '10 Year Anniversary - Magenta', 6),
+    ('Diamond', 'Opto', 'LATITUDE 64°', 158, 8, 6, -3, 1, 'Fairway Driver', 'Side Bag', 'Red', 'Blue', 6),
+    ('FD', 'S-Blend', 'DISCMANIA', 173, 7, 6, -1, 1, 'Fairway Driver', 'Side Bag', 'Blue', 'California/Colorado Stamp - Blue', 6),
+    ('Monarch', 'Champion', 'INNOVA DISC GOLF', 175, 10, 5, -4, 1, 'Control Driver', 'Side Bag', 'Orange', 'Silver', 6),
+    ('Bolt', 'Gold Line Orbit', 'LATITUDE 64°', 175, 13, 6, -2, 3, 'Distance Driver', 'Side Bag', 'Pink', '10 Year Anniversary - Magenta', 6),
+    ('Enforcer', 'Fuzion Bio', 'DYNAMIC DISCS', 172, 12, 4, 0.5, 4, 'Distance Driver', 'Side Bag', 'Yellow', 'Blue', 7),
 
 
 
+    ('Underworld', 'Vip', 'WESTSIDE DISCS', 173, 7, 6, -3, 1, 'Fairway Driver', 'Backup', 'Green', 'Silver', 6),
+    ('Diamond', 'Opto Air', 'LATITUDE 64°', 139, 8, 6, -3, 1, 'Fairway Driver', 'Backup', 'Teal', 'Bronze', 6),
+    ('River', 'Opto', 'LATITUDE 64°', 174, 7, 7, -1, 1, 'Fairway Driver', 'Backup', 'Pink Glitter', 'John E McCray - Black', 7),
+    ('River', 'Opto', 'LATITUDE 64°', 173, 7, 7, -1, 1, 'Fairway Driver', 'Backup', 'White Glitter', '10 Year Anniversary = Magenta Dots', 7),
+    ('Teebird', 'Star Glow', 'INNOVA DISC GOLF', 173, 7, 5, 0, 2, 'Fairway Driver', 'Backup', 'Purple', 'Jen Allen Tour Series - Blue', 7),
 
-    ('Diamond', 'Opto Air', 'LATITUDE 64°', 139, 8, 6, -3, 1, 'Fairway Driver', 'Backup', 'Teal', 'Bronze', 5),
+
+    ('Road Runner', 'Star', 'INNOVA DISC GOLF', 167, 9, 5, -4, 1, 'Control Driver', 'Backup', 'Yellow', 'Black', 6),
+    ('TD', 'C-Line', 'DISCMANIA', 175, 10, 5, -2, 1, 'Control Driver', 'Backup', 'Clear-Yellow', 'Gold', 6),
+    ('Enemy', 'Star Plastic', 'Unknown', 169, 9, 3, 0, 4, 'Control Driver', 'Backup', 'Blue', 'Bronze', 7),
+    ('Felon', 'Lucid Air', 'LATITUDE 64°', 159, 9, 3, 0.5, 4, 'Control Driver', 'Backup', 'Orange', 'Blue-Green', 7),
 
 
+    ('Mamba', 'Star', 'INNOVA DISC GOLF', 171, 11, 6, -5, 1, 'Distance Driver', 'Backup', 'Red', 'Rainbow Blend', 6),
+    ('Mamba', 'Champion', 'INNOVA DISC GOLF', 174, 11, 6, -5, 1, 'Distance Driver', 'Backup', 'Yellow', 'Gold', 5),
     ('Wraith', 'Star', 'INNOVA DISC GOLF', 162, 11, 5, -1, 3, 'Distance Driver', 'Backup', 'Orange', '12 Time World Champion Ken Climo - Black', 6),
     ('Wraith', 'Star', 'INNOVA DISC GOLF', 175, 11, 5, -1, 3, 'Distance Driver', 'Backup', 'Light Orange', 'No Stamp', 6),
     ('Tern', 'Star', 'INNOVA DISC GOLF', 170, 12, 6, -2, 2, 'Distance Driver', 'Backup', 'Orange', 'Santa Cruz Hand Mouth', 7),
@@ -69,7 +91,6 @@ VALUES
     ('Harp', 'BT Medium', 'WESTSIDE DISCS', 173, 4, 3, 0, 3, 'Putter', 'Loan', 'Blue', 'Physics of Flight - Gold', 7),
     ('Harp', 'UNKNOWN', 'WESTSIDE DISCS', 176, 4, 3, 0, 3, 'Putter', 'Loan', 'Clear', 'Ricky Wysocki 2x World Champion - Red', 7),
     ('Enforcer', 'Lucid', 'DYNAMIC DISCS', 172, 12, 4, 0.5, 4, 'Distance Driver', 'Loan', 'Blue', 'Gold Woodgrain', 7),
-    ('Enforcer', 'Fuzion Bio', 'DYNAMIC DISCS', 172, 12, 4, 0.5, 4, 'Distance Driver', 'Loan', 'Yellow', 'Blue', 7),
     ('Ballista Pro', 'Opto', 'LATITUDE 64°', 174, 14, 4, 0, 3, 'Distance Driver', 'Loan', 'Pink Glimmer', `PDGA Pro Worlds '21 - Black`, 7),
     ('Defender', 'Fuzion', 'DYNAMIC DISCS', 172, 13, 5, 0, 3, 'Distance Driver', 'Loan', 'Orange', 'Silver Holographic', 7),
     

c1c0bceabfd9fc38fe09c9537766f0f0d1b82ab4
lunettas <silaslunetta@gmail.com>
2023-01-12 22:03:45 -0700

    setting up a new branch for node

--- silas_front_end/app.js
+++ silas_front_end/app.js
@@ -9,24 +9,25 @@ const server = http.createServer((req, res) => {
   res.end('Welcome to discsDB');
 });
 
+var mysql      = require('mysql2');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'iamhackerman'
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 
 
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'example.org',
-//   user     : 'bob',
-//   password : 'secret'
-// });
- 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('error connecting: ' + err.stack);
-//     return;
-//   }
- 
-//   console.log('connected as id ' + connection.threadId);
-// });

89908d5d9c3cfaf7fd2303ac6ce7a6af4ec5333b
lunettas <silaslunetta@gmail.com>
2023-01-12 21:31:11 -0700

    better sorting for the query

--- Silas Queries.sql
+++ Silas Queries.sql
@@ -11,7 +11,7 @@ ORDER BY Stability, Weight,`Sleepy Scale`;
 
 -- Beefy 
 SELECT DISTINCT Mold, Speed, Glide, Turn, Fade, `Sleepy Scale`, Weight, Stability FROM
-(SELECT Mold, Speed, Glide, Turn, Fade, `Sleepy Scale`, Weight, (Turn + Fade) AS Stability  FROM silasDiscs ORDER BY Stability) AS calStability
+(SELECT Mold, Speed, Glide, Turn, Fade, `Sleepy Scale`, Weight,   FROM silasDiscs ORDER BY Stability) AS calStability
 WHERE Weight !='Null'
 ORDER BY Stability DESC, Weight DESC,`Sleepy Scale` DESC;
 
@@ -43,7 +43,8 @@ SELECT Mold, COUNT(*) AS Count FROM
 GROUP BY Mold ORDER BY Count DESC;
 
 -- query for discs I lent josh 
-SELECT Mold, Speed, Glide, Turn, Fade, `Sleepy Scale`, Category
+SELECT Mold, Speed, Glide, Turn, Fade,(Turn + Fade) AS Stability, `Sleepy Scale`, Category
 FROM silasDiscs 
-WHERE ID IN(223, 232, 233, 231, 238, 159, 222, 171, 155, 154. 204, 203, 33, 200, 230, 111, 218, 120, 221)
+WHERE ID IN(223, 232, 233, 231, 238, 159, 222, 171, 155, 154, 204, 203, 33, 200, 230, 111, 218, 120, 221)
+ORDER BY Speed, Stability;
 -- 238 wrong color? idk if the BD beast or roc3 are logged...
--- silas_front_end/app.js
+++ silas_front_end/app.js
@@ -12,21 +12,21 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  ssl  : {
-    ca : fs.readFileSync(__dirname + '/mysql-ca.crt')
-  }
-  user     : 'root',
-  password : ''
-});
+
+
+
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'example.org',
+//   user     : 'bob',
+//   password : 'secret'
+// });
  
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
  
-  console.log('connected as id ' + connection.threadId);
-});
+//   console.log('connected as id ' + connection.threadId);
+// });

5debb6062d023e29c6a8c6f0c06785d1de50fe36
lunettas <silaslunetta@gmail.com>
2023-01-14 00:12:17 -0700

    removed a dupe (#5)
    * removed a dupe
    
    * added queries weight/mold union with joshdiscs
    
    * Added final discs and fixed some entries
    
    * flight numbersss
    
    * better stability #'s
    
    * typo
    
    * updating justices saying goodbye to explorer
    
    * even better :)
    
    Co-authored-by: Josh Cabral <55261816+joshycsm@users.noreply.github.com>

--- Silas Queries.sql
+++ Silas Queries.sql
@@ -14,3 +14,31 @@ SELECT DISTINCT Mold, Speed, Glide, Turn, Fade, `Sleepy Scale`, Weight, Stabilit
 (SELECT Mold, Speed, Glide, Turn, Fade, `Sleepy Scale`, Weight, (Turn + Fade) AS Stability  FROM silasDiscs ORDER BY Stability) AS calStability
 WHERE Weight !='Null'
 ORDER BY Stability DESC, Weight DESC,`Sleepy Scale` DESC;
+
+-- Basic UNION
+Select * From silasdiscs UNION ALL Select * FROM jcdiscs;
+
+-- Find lightweight 
+Select * From silasdiscs WHERE  Weight <=169  UNION ALL Select * FROM jcdiscs WHERE  Weight <=169;
+
+-- Flight numbers
+Select DISTINCT Mold, Speed, Glide, Turn, Fade, (Turn + Fade) AS Stability From silasdiscs
+ORDER BY Speed, Stability
+;
+
+Select DISTINCT Mold, Speed, Glide, Turn, Fade, (Turn + Fade) AS Stability From silasdiscs
+UNION 
+Select DISTINCT Mold, Speed, Glide, Turn, Fade, (Turn + Fade) AS Stability From jcdiscs
+ORDER BY Speed, Stability
+;
+
+SELECT Mold, COUNT(*) AS Count FROM silasdiscs 
+GROUP BY Mold ORDER BY Count DESC LIMIT 5;
+
+SELECT Mold, COUNT(*) AS Count FROM jcdiscs
+GROUP BY Mold ORDER BY Count DESC LIMIT 5;
+
+SELECT Mold, COUNT(*) AS Count FROM
+(SELECT * FROM silasdiscs UNION ALL Select * FROM jcdiscs) AS totalDiscs
+GROUP BY Mold ORDER BY Count DESC;
+

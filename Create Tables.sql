984e2f977008c3adfc703395e927fc0305c72b1a
Josh Cabral <55261816+joshycsm@users.noreply.github.com>
2023-01-14 01:46:57 -0700

    Enhancements 0.2 (#7)
    * working code :)
    
    * working code :+1:
    
    * still working :8ball:  :)
    
    * working code :')
    
    * commit for silas :) working code :+1:
    
    * Ready for code review! :)
    
    * inventory nearly completely. New discs left to input.
    
    * best query added x) :)

--- Create Tables.sql
+++ Create Tables.sql
@@ -31,7 +31,7 @@ Glide INT,
 Turn DECIMAL(2,1),
 Fade DECIMAL(2,1),
 Slot ENUM( 'Putter', 'Mid-Range', 'Fairway Driver' , 'Control Driver', 'Distance Driver'),
-Category ENUM('Main Bag', 'Side Bag', 'Collection', 'Backup', 'Emily', 'Sale / Trade', 'Loan', 'Dalton', 'Silas'),
+Category ENUM('Main Bag', 'Side Bag', 'Collection', 'Backup', 'Emily', 'Sale / Trade', 'Loan', 'Dalton', 'Max', 'Silas'),
 Color VARCHAR(100),
 Stamp VARCHAR(100),
 `Sleepy Scale` INT

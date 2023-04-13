efd12cca9a1db1f844c8fd00a0608f27d2140134
lunettas <silaslunetta@gmail.com>
2023-04-13 14:42:51 -0600

    Revert "Revert "messing with a flight chart""
    This reverts commit 202dd043b06b69e94119969245525e6a95b8946c.

---
+++ silas_front_end/app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const { localHostConnection, connection } = require('./db.js');
+const {engine} = require('express-handlebars');
+const https = require('https');
+const querystring = require('querystring');
+const mime = require('mime');
+
+const app = express();
+const port = 3000;
+
+app.use('/styles.css', (req, res, next) => {
+  res.setHeader('Content-Type', 'text/css');
+  const filePath = path.join(__dirname, 'styles.css');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      next(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+//handlebars routing
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+  });
+app.get('/input', (req, res) => {
+    res.render('input');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+app.listen(port, function (){
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+// parse incoming form data
+
+app.use(express.urlencoded({ extended: true }));
+
+// Define the file path for storing form submissions
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const filePath = path.join(__dirname, 'form-submissions.txt');
+
+
+
+// handle form submission
+app.post('/submit', async (req, res) => {
+    const { table, mold, plastic, brand, weight, speed, glide, turn, fade, category, color, stamp, sleepyscale } = req.body;
+    console.log('Received form input:', req.body);
+       let slot;
+    if (speed>0 && speed <=4){slot = 'Putter';} else if (speed>4 && speed<7){slot = 'Mid-Range';}else if(speed>6&&speed<9){slot='Fairway Driver';}else if(speed>=9&&speed<11){slot = 'Control Driver';}else if(speed>=11){slot='Distance Driver';}    
+ try {
+      const conn = await connection();
+      const result = await conn.execute(
+            'INSERT INTO '+table+'(Mold, Plastic, Brand, Weight, Speed, Glide, Turn, Fade, Slot, Category, Color, Stamp, `Sleepy Scale`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [ mold ?? null, plastic ?? null,        brand ?? null,        weight ?? null,        speed ?? null,        glide ?? null,        turn ?? null,        fade ?? null,        slot ?? null,        category ?? null,        color ?? null,        stamp ?? null,        sleepyscale ?? null
+        ]
+      );
+      
+      
+      //Write formData to file
+      const formData = `('${mold}', '${plastic}', '${brand}', ${weight}, ${speed}, ${glide}, ${turn}, ${fade}, '${slot}', '${category}', ${color}', '${stamp}', ${sleepyscale}),\n`;
+    fs.appendFile(filePath, formData, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error storing form submission');
+      } else {
+        console.log('Disc added to the database and form submission stored!');
+        res.redirect('/input');
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error adding disc to the database');
+  }
+});
+

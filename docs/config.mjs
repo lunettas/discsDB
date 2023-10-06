fc7139baf3b09b3a2cc7874f3dfde36f88a00796
lunettas <silaslunetta@gmail.com>
2023-10-06 16:15:08 -0600

    Post deployment edits 1.2 (#14)
    * pulled main to get latest code base
    
    * updated .env to have a .env.dev and .env.prod
    
    * started implementing changelog on /about page
    
    * testing git
    
    * Improved changelog, added about me + color palette
    
    * improved styling, implemented new colors
    
    * improved styling, login form, and changed .env
    
    * updated changelog

---
+++ docs/config.mjs
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+const envPath = path.join(process.cwd(), '.env.prod');
+
+dotenv.config({ path: envPath });
+
+// Access the environment variables and export them
+export const DATABASE = process.env.DATABASE;
+export const HOST = process.env.HOST;
+export const USERNAME = process.env.USERNAME;
+export const PASSWORD = process.env.PASSWORD;
+export const SESSION_SECRET = process.env.SESSION_SECRET;
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+export const SERVER_IP_ADDRESS = process.env.SERVER_IP_ADDRESS;
+export const SSL_KEY_PATH = process.env.SSL_KEY_PATH;
+export const SSL_CERT_PATH = process.env.SSL_CERT_PATH;
+export const SSL_CA_PATH = process.env.SSL_CA_PATH;
+export const PORT = process.env.PORT;
